08f3120c3da5a97adb86d42bc6c589311e48f5da
Avram Stern <stern9@gmail.com>
2015-03-25 02:03:17 -0600

    Initial Commit
    Add files

---
+++ js/alternate.js
@@ -0,0 +1,9 @@
+$.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+          {
+            tags: animal,
+            format: "json"
+          },
+          function (data) {
+          
+          }
+         );
